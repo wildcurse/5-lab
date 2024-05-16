05615af2f008a2808f28cfc4ec642bce00a8cbc8
wildcurse <sxlah@mail.ru>
2024-05-16 18:41:08 +0300

    Add files via upload

---
+++ 5.7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите пять чисел:");
+        string input = Console.ReadLine();
+
+        // Преобразуем введенные данные в массив чисел
+        int[] numbers = ParseNumbers(input);
+
+        if (numbers.Length != 5)
+        {
+            Console.WriteLine("Ошибка: Пожалуйста, введите ровно пять чисел.");
+            return;
+        }
+
+        // Находим максимальное и минимальное число
+        int maxNumber = numbers.Max();
+        int minNumber = numbers.Min();
+
+        Console.WriteLine($"Максимальное число: {maxNumber}");
+        Console.WriteLine($"Минимальное число: {minNumber}");
+    }
+
+    static int[] ParseNumbers(string input)
+    {
+        // Разделяем строку на части и преобразуем в массив чисел
+        string[] parts = input.Split(' ');
+        int[] numbers = new int[parts.Length];
+        for (int i = 0; i < parts.Length; i++)
+        {
+            numbers[i] = int.Parse(parts[i]);
+        }
+        return numbers;
+    }
+}

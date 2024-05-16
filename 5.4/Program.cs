a2edf879da4fff52cc6faba50c670b1b0be94304
wildcurse <sxlah@mail.ru>
2024-05-16 18:38:28 +0300

    Add files via upload

---
+++ 5.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите путь к файлу:");
+        string filePath = Console.ReadLine();
+
+        string fileName = GetFileNameFromPath(filePath);
+
+        Console.WriteLine($"\nИмя файла: {fileName}");
+    }
+
+    static string GetFileNameFromPath(string path)
+    {
+        // Разделяем путь по символу разделителя директорий
+        string[] pathParts = path.Split(System.IO.Path.DirectorySeparatorChar);
+
+        // Имя файла будет последним элементом массива
+        return pathParts[pathParts.Length - 1];
+    }
+}

62506ba3544d48dc2e67eb98db57a2a6652b9db1
wildcurse <sxlah@mail.ru>
2024-05-16 18:38:50 +0300

    Add files via upload

---
+++ 5.5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите предложение:");
+        string sentence = Console.ReadLine();
+
+        string longestWord = FindLongestWord(sentence);
+
+        Console.WriteLine($"\nСамое длинное слово: {longestWord}");
+    }
+
+    static string FindLongestWord(string sentence)
+    {
+        // Разделяем предложение на слова
+        string[] words = sentence.Split(' ');
+
+        string longestWord = "";
+        int maxLength = 0;
+
+        // Ищем самое длинное слово
+        foreach (string word in words)
+        {
+            if (word.Length > maxLength)
+            {
+                maxLength = word.Length;
+                longestWord = word;
+            }
+        }
+
+        return longestWord;
+    }
+}

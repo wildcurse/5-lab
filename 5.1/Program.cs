b4f34684664c090354e51b352070e760bb9c883e
wildcurse <sxlah@mail.ru>
2024-05-16 18:05:42 +0300

    Add files via upload

---
+++ 5.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // запрашиваем и получаем первое число
+        Console.Write("Enter the First number:\n>");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+        // запрашиваем и получаем второе число
+        Console.Write("Enter the Second number:\n>");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+        // запрашиваем и получаем третье число
+        Console.Write("Enter the third number:\n>");
+        double thirdNumber = Convert.ToDouble(Console.ReadLine());
+
+        // запрашиваем и получаем четвертое число
+        Console.Write("Enter the four number:\n>");
+        double fourthNumber = Convert.ToDouble(Console.ReadLine());
+
+        // вычисляем среднее значение
+        double average = CalculateAverage(firstNumber, secondNumber, thirdNumber, fourthNumber);
+
+        // вводим результат
+        Console.WriteLine($"The average of {firstNumber}, {secondNumber}, {thirdNumber}, {fourthNumber} is: {average}");
+    }
+
+    static double CalculateAverage(double num1, double num2, double num3, double num4)
+    {
+        double sum = num1 + num2 + num3 + num4;
+        return sum / 4;
+    }
+}

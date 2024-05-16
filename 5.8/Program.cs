fc5e9b4bc22fc5b765bc71bebca311e865cae7ee
wildcurse <sxlah@mail.ru>
2024-05-16 18:45:19 +0300

    Add files via upload

---
+++ 5.8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество ступеней:");
+        int levels;
+
+        while (!int.TryParse(Console.ReadLine(), out levels) || levels <= 0)
+        {
+            Console.WriteLine("Пожалуйста, введите корректное положительное целое число.");
+        }
+
+        PrintNumberPyramid(levels);
+    }
+
+    static void PrintNumberPyramid(int levels)
+    {
+        for (int i = 1; i <= levels; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+            Console.WriteLine();
+        }
+    }
+}
